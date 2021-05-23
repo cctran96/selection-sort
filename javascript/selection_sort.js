04abb97cd5ee3a0677c130b40d22f2921a4c8ecb
Chau Tran <cctran96@gmail.com>
2021-05-22 21:51:35 -0700

    Solved

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,6 +1,34 @@
+const {performance} = require("perf_hooks")
+
 function selectionSort(arr) {
-  // type your code here
+  // 0.0004917839765548706s
+  let sorted = []
+  while (arr.length > 0) {
+    let min = Math.min(...arr)
+    let index = arr.indexOf(min)
+    sorted.push(min)
+    arr.splice(index, 1)
+  }
+  return sorted
+
+  // 0.0059714454412460325s
+  // let n = arr.length
+  // for(let i = 0; i < n; i++) {
+  //     let min = i
+  //     for(let j = i+1; j < n; j++){
+  //         if(arr[j] < arr[min]) {
+  //             min = j
+  //         }
+  //      }
+  //      if (min != i) {
+  //          let tmp = arr[i]
+  //          arr[i] = arr[min]
+  //          arr[min] = tmp
+  //     }
+  // }
+  // return arr
 }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -15,6 +43,13 @@ if (require.main === module) {
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+  let t0 = performance.now()
+  for (let i = 0; i < 1000; i++) {
+    selectionSort([3,2,1])
+    selectionSort(longInput)
+  }
+  let t1 = performance.now()
+  console.log((t1-t0)/2000)
 }
 
 module.exports = selectionSort;
--- ruby/selection_sort.rb
+++ ruby/selection_sort.rb
@@ -1,5 +1,30 @@
 def selection_sort(arr)
-  # type your code in here
+  n = arr.length - 1
+
+  # 0.0001423015s
+  # for i in 0..n
+  #   min = i
+  #   for j in (i+1)..n
+  #     if arr[j] < arr[min]
+  #       arr[j], arr[min] = arr[min], arr[j]
+  #     end
+  #   end
+  # end
+  # arr
+
+  # 0.0001356495s
+  n.times do |i|
+    min = i
+    for j in (i+1)..n
+      if arr[j] < arr[min]
+        min = j
+      end
+    end
+    if min != i 
+      arr[i], arr[min] = arr[min], arr[i]
+    end
+  end
+  arr
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,6 +40,14 @@ if __FILE__ == $PROGRAM_NAME
   long_input = []
 
   100.times { long_input << rand }
+  
+  t0 = Time.now
+  1000.times do
+    selection_sort([3,2,1])
+    selection_sort(long_input)
+  end
+  t1 = Time.now
+  puts (t1 - t0)/2000
 end
 
 # Please add your pseudocode to this file
